bc1e859a23c416b808d76882690272a78c5d9276
Italo Ramon Campos <italo.ramon7@live.com>
2018-12-04 14:47:32 -0300

    Uploading initial operations

--- matrice-sum.c
+++ matrice-sum.c
@@ -1,34 +1,66 @@
-#include <mpi.h>
+//#include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#define ORDEM 10
+#define ORDEM 100
 
 int main(int argc, char** argv) {
 	// Inicia o ambiente do MPI
-	MPI_Init(NULL, NULL);
+	//MPI_Init(NULL, NULL);
 
-	// Encontra o tamanho do rank
-	int world_rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+	// Captura o número de processos que serão utilizados na execução
+	//int world_rank;
+	//MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-	int world_size;
-	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-	
+	// Captura o número total de processos
+	//int world_size;
+	//MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
 	int matrixA[ORDEM][ORDEM];
-	int matrixB[ORDEM][ORDEM];
-	int matrixC[ORDEM][ORDEM];
-
-	// Popula matrizes A e B
+	//int matrixB[ORDEM][ORDEM];
+	//int matrixC[ORDEM][ORDEM];
+	
+	// Povoa a matriz com 1's
 	for (int i = 0; i < ORDEM;  i++) {
 		for (int j = 0; j < ORDEM; j++) {
 			matrixA[i][j] = 1;
-			matrixB[i][j] = 1;
+			//matrixB[i][j] = 1;
 		}	
 	}
+
+	// Verifica o número de processos
+	//if(world_size != 4) {
+	//	fprintf(stderr, "Two proccess are needed only\n");
+	//	MPI_Abort(MPI_COMM_WORLD, 1);
+	//}
+
+	for (int i = 1; i < ORDEM;  i+=4) {
+		for (int j = 1; j < ORDEM; j+=4) {
+			printf("%d ", matrixA[i][j]);
+			//matrixB[i][j] = 1;
+		}
+		printf("\n");
+	}
+
+	/*if(world_rank == 0) {
+		vector[0] = 1;
+		vector[1] = 2;
+		vector[2] = 3;
+		MPI_Send(&vector, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+	}
+	else {
+		if (world_rank == 1) {
+			MPI_Recv(&pointer, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			printf("Process 1 received arg from process 0\n");
+			int i;
+			for(i = 0; i < 3; pointer++)
+				printf("%d ", *pointer);
+		}
+	}*/
+
 	 
 	// Para executar essas tarefas existem quatro ranks
-	if (world_rank == 0) {
+	/*if (world_rank != 0) {
 		int c1[ORDEM/4][ORDEM/4];
 		for (int i = 0; i < 24; i++) {
 			for (int j = 0; j < 24; i++) {
@@ -43,6 +75,6 @@ int main(int argc, char** argv) {
 	if (world_rank == 1) {
 	}
 	if (world_rank == 1) {
-	}
-	MPI_Finalize();
+	}*/
+	//MPI_Finalize();
 }
